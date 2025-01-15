7f121e5554352dce770f803b0e8c356815d6eb56
qub3 <g.eckardt@protonmail.com>
2025-01-15 15:54:05 +0100

    avx2

--- src/simd/vm_mul.c
+++ src/simd/vm_mul.c
@@ -3,7 +3,6 @@
 #include <stdbool.h>
 #include <time.h> 
 #include <flexiblas/cblas.h>
-#include <string.h>
 
 // https://github.com/srinathv/ImproveHpc/blob/master/intel/2015-compilerSamples/C%2B%2B/intrinsic_samples/intrin_dot_sample.c
 bool mat_vec_AVX2(int cols, int rows, float *matrix, float *vec_add, float *vec_mul, float *res){
@@ -12,30 +11,69 @@ bool mat_vec_AVX2(int cols, int rows, float *matrix, float *vec_add, float *vec_
         return false;
     }
 
+    float result;
     for( int i = 0; i < cols; i++){
+
         __m256 v_mat, v_vec, sum;
         __m128 top,bot;
         sum = _mm256_setzero_ps();  
+        v_vec = _mm256_loadu_ps(vec_mul + i);  
 
         for(int j = 0; j < rows; j += 8){
-            v_mat = _mm256_loadu_ps(matrix + cols * i + j);   
-            v_vec = _mm256_loadu_ps(vec_mul + j);  
+            v_mat = _mm256_loadu_ps(matrix + cols * j + i);   
+
             sum = _mm256_fmadd_ps(v_mat, v_vec, sum); 
+            sum = _mm256_hadd_ps(sum, sum); 
+
+            top = _mm256_extractf128_ps(sum,1);   
+            bot = _mm256_extractf128_ps(sum,0);  
+
+            top = _mm_add_ps(top,bot);    
+            top = _mm_hadd_ps(top,top);  
+            _mm_store_ss(&result,top);
+            printf("%f   ", result);
+
+
+            res[j] += result;
         }
 
-        sum = _mm256_hadd_ps(sum, sum); 
-
-        top = _mm256_extractf128_ps(sum,1);   
-        bot = _mm256_extractf128_ps(sum,0);  
-
-        top = _mm_add_ps(top,bot);    
-        top = _mm_hadd_ps(top,top);  
-
-        _mm_store_ss(res+i,top);
+        res[i] += vec_add[i];
     }
 
     return true;
 }
+
+//bool s_mat_vec_AVX2(int cols, int rows, float *matrix, float *vec_add, float *vec_mul, float *res){
+//    if( cols%8 != 0 || rows%8 != 0 ){
+//        printf("break");
+//        return false;
+//    }
+//
+//    __m256 v_mat, v_vec, sum;
+//    __m128 top,bot;
+//    sum = _mm256_setzero_ps();  
+//
+//    for( int i = 0; i < cols; i++){
+//        v_vec = _mm256_loadu_ps();  
+//        for(int j = 0; j < rows; j += 8){
+//            v_mat = _mm256_loadu_ps();   
+//            sum = _mm256_fmadd_ps(v_mat, v_vec, sum); 
+//        }
+//
+//        sum = _mm256_hadd_ps(sum, sum); 
+//
+//        top = _mm256_extractf128_ps(sum,1);   
+//        bot = _mm256_extractf128_ps(sum,0);  
+//
+//        top = _mm_add_ps(top,bot);    
+//        top = _mm_hadd_ps(top,top);  
+//
+//        _mm_store_ss(res+i,top);
+//        res[i] += vec_add[i];
+//    }
+//
+//    return true;
+//}
 
 bool naive_algo(int cols, int rows, float *matrix, float *vec_add, float *vec_mul, float *res){
     if( cols%8 != 0 || rows%8 != 0 ){
@@ -76,7 +114,7 @@ void reset_values(float *ptr,int values){
 int main() {
     printf("compiled...\n");
 
-    int s = 100;
+    int s = 8;
 
     float *vec_mul;
     float *vec_add;
@@ -104,7 +142,9 @@ int main() {
     //print(vec_add, 1, s);
 
     // reset_values
+    reset_values(vec_mul, s);
     reset_values(vec_add, s);
+    reset_values(matrix, s*s);
 
     t = clock(); 
     mat_vec_AVX2(s, s, matrix, vec_mul, vec_add, res); 
@@ -112,8 +152,10 @@ int main() {
     time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds 
     printf("AVX2:  %f \n", time_taken);
 
-    //print(res, 1, s);
-    memset(res, (float)0, s);
+    print(res, 1, s);
+    for(int i = 0; i < s; i++){
+        res[i] = 0;
+    }
 
     // reset_values
     reset_values(vec_mul, s);
@@ -126,6 +168,5 @@ int main() {
     time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds 
     printf("Naive: %f \n", time_taken);
 
-
-    //print(res, 1, s);
+    print(res, 1, s);
 }
